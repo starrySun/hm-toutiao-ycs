7f4721483e3a0543a4b47888ede9f6879abaec67
nick <455105593@qq.com>
2019-08-01 21:53:28 +0800

    初始化项目

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
+// main.jsn 入口文件
+// 职责1：引入需要依赖的模块 （js ,包,css,其他资源）
+// 职责2:创建一个根Vue 实例
 import Vue from "vue";
 import App from "./App.vue";
 
+//产品级别提示  不开启 开发阶段 提示越详细越好 开发级别提示
 Vue.config.productionTip = false;
 
 new Vue({
